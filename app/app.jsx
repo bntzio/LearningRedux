37124c79b8d700c76ade5226cf922e6227ba80be
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-07-31 19:47:47 -0500

    Initial commit - Install redux

---
+++ app/app.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+// Components
+
+// Load Foundation
+$(document).foundation();
+
+// Load Custom Styles
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <p>LearningRedux</p>,
+  document.getElementById('app')
+);
+
+require('./redux-example.jsx');
